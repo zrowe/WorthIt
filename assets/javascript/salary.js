bb0899c1e8b1fe2d4651bbac3e6f7b305c7d0350
Rhonda Glennon <rhondaglennon@gmail.com>
2018-02-08 11:38:06 -0800

    update functions from code review

--- assets/javascript/salary.js
+++ assets/javascript/salary.js
@@ -1,45 +1,42 @@
 // create an object of job titles and salaries (source: Glassdoor)
 var salary = {
-  "software engineer" : 124555,
-  "QA engineer" : 85292,
-  "product manager" : 129772,
-  "devops engineer" : 143779,
   "backend engineer" : 84864,
-  "frontend engineer" : 115109,
   "data scientist" : 142729,
+  "devops engineer" : 143779,
+  "frontend engineer" : 115109,
+  "product manager" : 129772,
+  "QA engineer" : 85292,
   "senior software engineer" : 144045
+  "software engineer" : 124555,
 }
 
-var salaryMonthly;
-var salary25;
-var salary30;
-var salary35;
+// function displayPositions(jobTitle) {
+//   for (key in salary) {
+//     console.log(key);
+//   }
+// }
+
 
 // calculate monthly salary (annual / 12 months), rounded to integer
 function calcMonthlySalary(salary) {
-  salaryMonthly = Math.round(salary / 12);
-  console.log(salaryMonthly);
+  var salaryMonthly = Math.floor(salary / 12);
   return salaryMonthly;
 }
 
 // calculate percents of monthly salary (25, 30, and 35)
-function calcSalaryPercents(salary) {
-  var salary25 = Math.round(0.25 * salary / 12);
-  var salary30 = Math.round(0.30 * salary / 12);
-  var salary35 = Math.round(0.35 * salary / 12);
-  console.log("25 percent" + salary25);
-  console.log("30 percent" + salary30);
-  console.log("35 percent" + salary35);
-  return [salary25, salary30, salary35];
+function calcAffordRanges(salary) {
+  var low = Math.floor(0.25 * salary / 12);
+  var medium = Math.floor(0.30 * salary / 12);
+  var high = Math.floor(0.50 * salary / 12);
+  return [low, medium, high];
 }
 
 // display the salary values as a string in currency (US Dollar) format for the HTML page
 function displayAsDollars(salary) {
   var salaryFormatted = salary.toLocaleString("en-US", { style: "currency", currency: "USD" });
-  console.log(salaryFormatted);
   return salaryFormatted;
 }
 
 calcMonthlySalary(salary["software engineer"]);
-calcSalaryPercents(salary["software engineer"]);
+calcAffordRanges(salary["software engineer"]);
 displayAsDollars(salary["software engineer"]);
